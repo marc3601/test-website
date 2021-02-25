16183b90934c37a06d6a2928705f07b0102f683e
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-02-25 19:53:46 +0100

    experimental features

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const ip = process.env.IP || "localhost";
 const port = process.env.PORT || 3000;
 const path = require("path");
 const bodyParser = require("body-parser");
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
+const { hostname } = require("os");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 //ROUTES --------------------------------------------------------
 
@@ -19,8 +24,54 @@ app.get("/", function (req, res) {
   res.sendFile(path.resolve("build/index.html"));
 });
 
-app.get("*", function (req, res) {
-  res.sendFile(path.resolve("build/index.html"));
+let Logs = [];
+
+const readDirectory = (req, callback) => {
+  fs.readdir("./public/uploads", function (err, items) {
+    const links = items.map((item) => {
+      return {
+        musicSrc: "http://" + req.headers.host + "/uploads/" + item,
+        name: item,
+      };
+    });
+    Logs.push(links);
+    callback(Logs);
+    Logs = [];
+  });
+};
+
+app.get("/data", (req, res) => {
+  readDirectory(req, (logFiles) => {
+    res.send({ files: { ...logFiles } });
+  });
+});
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./public/uploads");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: fileStorageEngine,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == "audio/mpeg") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only audio files allowed"));
+    }
+  },
+});
+
+
+
+app.post("/data", upload.single("song"), (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
